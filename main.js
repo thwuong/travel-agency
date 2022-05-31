473ba729ee469258e038e65ec3afeb1141872aab
duong_thuong <duongthuong654@gmail.com>
2022-05-31 11:35:34 +0700

    slider tour

--- main.js
+++ main.js
@@ -28,7 +28,6 @@ function handleSliderPlace() {
       index--;
       if (index < 0) {
         index = maxLength;
-        console.log(index);
         placeItems.forEach((item) => {
           item.style = `transform : translateX(-${110 * index}%)`;
         });
@@ -40,4 +39,46 @@ function handleSliderPlace() {
     }
   }
 }
+function handleSliderTour() {
+  const btnPre = document.querySelector(".tour__pre");
+  const btnNext = document.querySelector(".tour__next");
+  const tourItems = document.querySelectorAll(".tour-item");
+  const maxLength = [...tourItems].length / 3 - 1;
+  var index = 0;
+  btnPre.addEventListener("click", function () {
+    changeSlider(-1);
+  });
+  btnNext.addEventListener("click", function () {
+    changeSlider(1);
+  });
+
+  function changeSlider(num) {
+    if (num === 1) {
+      index++;
+      if (index <= maxLength) {
+        tourItems.forEach((item) => {
+          item.style = `transform : translateX(-${315 * index}%)`;
+        });
+      } else {
+        index = 0;
+        tourItems.forEach((item) => {
+          item.style = `transform : translateX(-${315 * index}%)`;
+        });
+      }
+    } else {
+      index--;
+      if (index < 0) {
+        index = maxLength;
+        tourItems.forEach((item) => {
+          item.style = `transform : translateX(-${315 * index}%)`;
+        });
+      } else {
+        tourItems.forEach((item) => {
+          item.style = `transform : translateX(-${315 * index}%)`;
+        });
+      }
+    }
+  }
+}
 handleSliderPlace();
+handleSliderTour();
